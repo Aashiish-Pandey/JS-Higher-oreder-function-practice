f84edf4a7fd328653068cc0f1df8890eaca239bb
Aashiish-Pandey <ash006pandey@gmail.com>
2020-09-21 09:55:44 +0530

    forEach() method

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const companies = [
     {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
   ];
   const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+ 
+  companies.forEach((company) => {
 
-  for(let i= 0;i<companies.length; i++) {
-
-    console.log(companies[i])
-  }
+    console.log(company.name)
+  })
