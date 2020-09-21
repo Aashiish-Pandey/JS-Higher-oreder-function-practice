37375ead6a58f9b1db0fd979225dc3f614722696
Aashiish-Pandey <ash006pandey@gmail.com>
2020-09-21 18:59:57 +0530

    map() method

--- main.js
+++ main.js
@@ -1,44 +1,61 @@
 const companies = [
-    {name: "Company One", category: "Finance", start: 1981, end: 2003},
-    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
-    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
-    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
-    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
-    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
-    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
-    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
-  ];
-  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64,100, 32];
- 
-  // companies.forEach((company) => {
+  { name: "Company One", category: "Finance", start: 1981, end: 2003 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+];
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 100, 32];
 
-  //   console.log(company.name + " " + company.start)
-  // })
+// companies.forEach((company) => {
 
-  // without using filter method 
+//   console.log(company.name + " " + company.start)
+// })
 
-  // canDrink = []
-  
-  // for(let i =0; i<ages.length; i++ ) {
+// without using filter method
 
-  //   if (ages[i] >=21) {
-  //     canDrink.push(ages[i])
-  //   }
-  // }
+// canDrink = []
 
-  // console.log(canDrink)
+// for(let i =0; i<ages.length; i++ ) {
 
-  canDrink = ages.filter(age => age>21)
-  console.log(canDrink)
+//   if (ages[i] >=21) {
+//     canDrink.push(ages[i])
+//   }
+// }
 
-  //Filter Retail companyies: 
+// console.log(canDrink)
 
-  retailComp = companies.filter(company =>company.category === "Retail")
-  console.log(retailComp)
+canDrink = ages.filter((age) => age > 21);
+console.log(canDrink);
 
-  // 80's companies
+//Filter Retail companyies:
 
-  eightiesCompnany = companies.filter(company => company.start >=1980 && company.strat<1990)
+retailComp = companies.filter((company) => company.category === "Retail");
+console.log(retailComp);
 
-  console.log(eightiesCompnany)
+// 80's companies
+
+eightiesCompnany = companies.filter(
+  (company) => company.start >= 1980 && company.strat < 1990
+);
+
+console.log(eightiesCompnany);
+
+//map() method ,create array of company name
+
+const companyName = companies.map((company) => company.name);
+console.log(companyName);
+
+const temp = companies.map((company) => {
+  return `${company.name} [${company.start}- ${company.end}]`;
+});
+console.log(temp);
+
+
+const ageTimesTwo = ages.map(age=>age*2)
+console.log(ageTimesTwo
+  )
